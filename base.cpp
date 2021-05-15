42f6fa0fc8f25aebacc3c7313f3dcbc7a2efbcf4
Grim97 <naveenjose97x@outlook.com>
2021-05-16 01:33:39 +0530

    Edge detection base code

--- base.cpp
+++ base.cpp
@@ -6,11 +6,25 @@
 using namespace cv;
 using namespace std;
 
+int edgedetection(Mat imagefile)
+{
+	Mat gray, gblur, canny;
+	cvtColor(imagefile, gray, COLOR_BGR2GRAY);
+	GaussianBlur(gray, gblur, Size(11, 11), 0, 0);
+	Canny(gblur, canny, 40, 50);
+	imshow("Grayscale", gray);
+	imshow("Gaussian blur", gblur);
+	imshow("Canny", canny);
+	waitKey(0);
+	return 0;
+}
+
 int main()
 {
-	string path = "include your path here";
+	string path = "C:\\Users\\navee\\OneDrive\\Documents\\Naveen\\Lane.jpg";
 	Mat imge = imread(path);
 	imshow("Laneimage", imge);
-	waitKey(0);
+	//waitKey(50);
+	edgedetection(imge);
 	return 0;
 }
